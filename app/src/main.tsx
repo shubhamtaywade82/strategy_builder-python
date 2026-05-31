0fc93d1bfcb032283d8b90ec3925991cc6f72f69
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-05-31 15:26:26 +0530

    refactor(frontend): swap TRPCProvider for AppQueryProvider

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
-import { TRPCProvider } from "@/providers/trpc"
+import { AppQueryProvider } from "@/providers/query"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <TRPCProvider>
+    <AppQueryProvider>
       <App />
-    </TRPCProvider>
+    </AppQueryProvider>
   </BrowserRouter>,
 )
